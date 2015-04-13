226c3f01a269edccf440499e580da68c19c091c8
freecates <ramongil@ramongil.cat>
2015-04-13 15:05:29 +0200

    nova data d'inici.

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -4,7 +4,7 @@ function initializeSirius() {
 	"use strict";
 
 	//COUNTDOWN
-	$('.countdown').countdown('2015/07/01', function(event) {
+	$('.countdown').countdown('2015/07/08', function(event) {
      	$(this).html(event.strftime('<div class="counter-column"><span class="count-digit">%D</span><span class="count-name">dies</span></div><div class="counter-column"><span class="count-digit">%H</span><span class="count-name">hores</span> </div><div class="counter-column"><span class="count-digit">%M</span> <span class="count-name">minuts</span></div><div class="counter-column"><span class="count-digit">%S</span><span class="count-name">segons</span></div>'));
 	});
 
